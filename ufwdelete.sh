8f7326806aa73c3d8585c96a0b391caed5601f05
Even <even.juberg@ntnu.no>
2020-02-06 13:55:02 +0100

    Added some comments.

--- ufwdelete.sh
+++ ufwdelete.sh
@@ -2,9 +2,12 @@
 input="test.list"
 while IFS= read -r line
 do
+#Finner regler med IP-ene som er listet i test.list
 number=$(ufw status numbered | grep $line)
 #echo $number
+#Finner nummeret regelen har, i UFW
 remove=$(echo $number | grep -o -E '[0-9]+' | head -1 | sed -e 's/^0\+//')
 #echo $remove
+#Sletter regelen og svarer ja
 yes | ufw delete $remove
 done < "$input"
